8e5c6d2af53e19fd07a9d361b3ff55a59a03aba3
Hemant Maru <hemantkumawat399@gmail.com>
2026-05-24 16:00:36 +0530

    fixed sitemap

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,7 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { VitePluginSitemap } from "vite-plugin-sitemap";
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(), tailwindcss(), VitePluginSitemap()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePluginSitemap({
+      hostname: "https://hemantmaru.vercel.app",
+    }),
+  ],
 });
